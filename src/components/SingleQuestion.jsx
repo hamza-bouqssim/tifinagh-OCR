4b55504ad259159d1ebcdd944f4376921507ae8d
Hamza Bouqssim <hbouqssi@e1r3p2.1337.ma>
2024-01-21 21:27:02 +0100

    FAQ section implemented

---
+++ src/components/SingleQuestion.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+const SingleQuestion = ({question, answer}) => {
+    const [showAnswer, setShowAnswer] = useState(false);
+    return (
+    <>
+        <div className='w-[80%] flex flex-col justify-center items-center'>
+            <div className='w-[100%] flex justify-between items-center'>
+                <h3 className='font-semibold text-darkBlue text-[30px]'>{question}</h3>
+                {
+                    showAnswer ? (
+                    <h3 className='font-semibold text-darkBlue text-[30px] cursor-pointer' onClick={() => setShowAnswer(false)}>-</h3>
+                    ): (
+
+                        <h3 className='font-semibold text-darkBlue text-[30px] cursor-pointer' onClick={() => setShowAnswer(true)}>+</h3>
+                    )
+                }
+            </div>
+            {
+                showAnswer ? (
+
+                    <p className='text-_gray font-bold'>{answer}</p>
+                ): null
+            }
+        </div>
+    </>
+  )
+}
+
+export default SingleQuestion
---
+++ src/utils/questions.js
@@ -0,0 +1,10 @@
+export const questions = [
+    {
+        question:"What is Tifinagh ?",
+        answer: "Tifinagh is an ancient script used primarily by the Tuareg people, who are a Berber ethnic group inhabiting parts of North Africa, including Mali, Niger, Algeria, Libya, Burkina Faso, and Chad. The script is one of the oldest known forms of Berber writing systems."
+    },
+    {
+        question: "How accurate is the image-to-text conversion for Tifinagh alphabets?",
+        answer: "Our picture-to-text tool keeps learning and getting better with new data. It tries its best to be accurate, but sometimes it might make a small mistake. We're always working to make it even better for you. We're always working to make it even better for you"
+    }
+]
