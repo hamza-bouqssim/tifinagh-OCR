d7d04d4e6da094c354ef79a49bd911b49deae19e
hamza-bouqssim <hamzaboukssim6@gmail.com>
2024-02-04 03:51:42 +0100

    fix: making FAQ section responsive

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -4,10 +4,10 @@ import { questions } from '../utils/questions'
 export const Faq = () => {
     const [cards, setCards] = useState(questions)
   return (
-    <div className=' w-full flex flex-col items-center pt-7'>
+    <div className=' w-full flex flex-col items-center pt-7 '>
         <h1 className='font-bold text-darkBlue text-[40px] text-center mb-[50px]'>Frequently Asked Questions</h1>
         
-        <div className='flex flex-col justify-center items-center gap-2 pb-[40px]'>
+        <div className='lg:w-[80%] flex flex-col justify-center items-center gap-2 pb-[40px] '>
             {cards.map((card, index) => (
                 <SingleQuestion {...card} key={index}/>
             ))}            
--- src/components/SingleQuestion.jsx
+++ src/components/SingleQuestion.jsx
@@ -4,18 +4,18 @@ const SingleQuestion = ({question, answer}) => {
     const [showAnswer, setShowAnswer] = useState(false);
     return (
     <>
-        <div className='w-[80%] flex flex-col justify-center items-center'>
+        <div className='w-[80%] flex flex-col justify-center items-center '>
             {
                 showAnswer ? (
 
-                <div className='w-[100%] flex justify-between items-center cursor-pointer'  onClick={() => setShowAnswer(false)}>
-                    <h3 className='font-semibold text-darkBlue text-[18px]'>{question}</h3>
-                    <h3 className='font-semibold text-darkBlue text-[18px]'>-</h3>
+                <div className='w-[80%] flex justify-between items-center cursor-pointer'  onClick={() => setShowAnswer(false)}>
+                    <h3 className='font-semibold text-darkBlue text-[18px] lg:text-[25px]'>{question}</h3>
+                    <h3 className='font-semibold text-darkBlue text-[18px] lg:text-[25px]'>-</h3>
                 </div>
                 ): (
-                <div className='w-[100%] flex justify-between items-center cursor-pointer'  onClick={() => setShowAnswer(true)}>
-                    <h3 className='font-semibold text-darkBlue text-[18px]'>{question}</h3>
-                    <h3 className='font-semibold text-darkBlue text-[18px]'>+</h3>
+                <div className='w-[80%] flex justify-between items-center cursor-pointer'  onClick={() => setShowAnswer(true)}>
+                    <h3 className='font-semibold text-darkBlue text-[18px] lg:text-[25px]'>{question}</h3>
+                    <h3 className='font-semibold text-darkBlue text-[18px] lg:text-[25px]'>+</h3>
                 </div>
                 )
             }
@@ -23,7 +23,7 @@ const SingleQuestion = ({question, answer}) => {
             {
                 showAnswer ? (
 
-                    <p className='text-_gray font-bold delay-1000'>{answer}</p>
+                    <p className='text-_gray font-bold  lg:w-[80%]'>{answer}</p>
                 ): null
             }
         </div>
